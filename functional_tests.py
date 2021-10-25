243cd0e528e09796982b133521267574125186b6
sachajoy <me.arihant.banthia@gmail.com>
2021-10-25 15:53:45 +0530

    Create first unit test and works fine

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,23 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+class NewVisitorsTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        # She notices the page title and header mention to-do lists
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+        
