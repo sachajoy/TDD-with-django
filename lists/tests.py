bf71bc1cf6698cf3947d7d1e2ade6e56ef34eca6
sachajoy <me.arihant.banthia@gmail.com>
2021-10-26 00:44:56 +0530

    Created test in for user input Added CSRF token in form Added functional for input

--- lists/tests.py
+++ lists/tests.py
@@ -12,6 +12,11 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_next': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
