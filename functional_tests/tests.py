89c6d8c6a6a90470e2523a97ba3bd3576eb25a2e
sachajoy <me.arihant.banthia@gmail.com>
2021-12-02 09:36:33 +0530

    move sqlite database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,12 +6,16 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 
 import time
+import os
 
 MAX_WAIT = 10
 
 class NewVisitorsTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
