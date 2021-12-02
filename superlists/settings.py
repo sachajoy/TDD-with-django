26d0c8eb0b594cada8ee988fdcd11e5c3e33d00c
sachajoy <me.arihant.banthia@gmail.com>
2021-12-02 10:16:34 +0530

    Added allowd hosts 139.59.61.43:8000

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s*edhz*8)v@w07!fvqg&dixutrry)^$fufb5@#(3z=1g-$q6wv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.59.61.43:8000']
 
 
 # Application definition
