fc6fe587e62f909d84d71f6d8b825ed280e068cf
sachajoy <me.arihant.banthia@gmail.com>
2021-12-09 10:17:18 +0530

    setting.py allowded host

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -10,6 +10,7 @@ def deploy():
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
+    print("Host: "+env.host)
     _update_settings(source_folder, env.host)
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
